93a3a8df2d5a1f8b53b2dca75aab3c38b68edbf5
Anon D <acework2u@gmail.com>
2023-06-19 10:02:30 +0700

    creat creds work:

--- go.mod
+++ go.mod
@@ -3,71 +3,72 @@ module github.com/acework2u/air-iot-app-api-service
 go 1.19
 
 require (
-	github.com/aws/aws-sdk-go v1.44.279
+	github.com/aws/aws-sdk-go v1.44.284
 	github.com/aws/aws-sdk-go-v2 v1.18.1
-	github.com/aws/aws-sdk-go-v2/config v1.18.25
-	github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider v1.22.10
+	github.com/aws/aws-sdk-go-v2/config v1.18.27
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.26
+	github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider v1.22.12
+	github.com/aws/aws-sdk-go-v2/service/iot v1.38.2
+	github.com/aws/aws-sdk-go-v2/service/sts v1.19.2
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/go-playground/validator/v10 v10.14.1
 	github.com/joho/godotenv v1.5.1
-	github.com/lestrrat-go/jwx v1.2.25
+	github.com/lestrrat-go/jwx v1.2.26
 	github.com/spf13/viper v1.16.0
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.0
 	github.com/swaggo/swag v1.16.1
-	go.mongodb.org/mongo-driver v1.11.6
+	go.mongodb.org/mongo-driver v1.11.7
 	go.uber.org/zap v1.24.0
-	golang.org/x/crypto v0.9.0
+	golang.org/x/crypto v0.10.0
 	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616
 )
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.13.24 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.3 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.4 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.34 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.28 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.34 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.35 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.28 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.12.10 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.10 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.19.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.12.12 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.12 // indirect
 	github.com/aws/smithy-go v1.13.5 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
-	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.0-20210816181553-5444fa50b93d // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
 	github.com/go-openapi/spec v0.20.9 // indirect
-	github.com/go-openapi/swag v0.22.3 // indirect
+	github.com/go-openapi/swag v0.22.4 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/golang/snappy v0.0.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/klauspost/compress v1.16.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
-	github.com/lestrrat-go/blackmagic v1.0.0 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
 	github.com/lestrrat-go/httpcc v1.0.1 // indirect
-	github.com/lestrrat-go/iter v1.0.1 // indirect
-	github.com/lestrrat-go/option v1.0.0 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/montanaflynn/stats v0.7.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/afero v1.9.5 // indirect
@@ -78,17 +79,17 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
-	github.com/xdg-go/scram v1.1.1 // indirect
-	github.com/xdg-go/stringprep v1.0.3 // indirect
-	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sync v0.2.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
-	golang.org/x/tools v0.9.3 // indirect
+	golang.org/x/net v0.11.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
+	golang.org/x/tools v0.10.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- services/things_service.go
+++ services/things_service.go
@@ -4,200 +4,132 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"strings"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
 
 	"github.com/aws/aws-sdk-go-v2/config"
+	cip "github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
+	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider/types"
+	"github.com/aws/aws-sdk-go-v2/service/iot"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	cognito "github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
-	"github.com/aws/aws-sdk-go/service/iot"
+	_ "github.com/aws/aws-sdk-go/service/iot"
+	"github.com/lestrrat-go/jwx/jwk"
+	"github.com/lestrrat-go/jwx/jwt"
 )
-
-//var ctx = context.TODO()
 
 type CogClient struct {
 	AppClientId string
 	UserPoolId  string
-	ClientCog   *cognito.CognitoIdentityProvider
-	Sess        *string
-	Iots        *iot.IoT
+	ClientCog   *cip.Client
+	IotClient   *iot.Client
+	StsSvc      *sts.Client
+	Cfg         *aws.Config
 }
 
 func NewThingClient(cognitoRegion string, userPoolId string, cognitoClientId string) ThinksService {
 
-	// cfg, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(cognitoRegion))
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	awsAccessKeyID := "AKIAXPA5O4YTL6NM4LKC"
-	awsSecretAccessKey := "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"
-
-	// Create an AWS session
-
-	// sess := session.Must(session.NewSessionWithOptions(session.Options{
-	// 	Config: aws.Config{
-	// 		Region: aws.String(cognitoRegion),
-	// 	},
-	// }))
-
-	// return &CogClient{
-	// 	AppClientId: cognitoClientId,
-	// 	UserPoolId:  userPoolId,
-	// 	ClientCog:   cip.NewFromConfig(cfg),
-	// }
-
-	// svc := iot.New(sess)
-
-	sess, err := session.NewSession(&aws.Config{
-		Region:      aws.String(cognitoRegion),
-		Credentials: credentials.NewStaticCredentials(awsAccessKeyID, awsSecretAccessKey, ""),
-	})
-
+	cfg, err := config.LoadDefaultConfig(context.Background(), config.WithRegion(cognitoRegion), config.WithSharedConfigProfile("air_iot_dev"))
 	if err != nil {
-		fmt.Println("Failed to create session:", err)
-
+		log.Fatalln("Failed to load AWS config:", err)
 	}
+	cognitoIdentityProviderClient := cip.NewFromConfig(cfg)
+	stsClient := sts.NewFromConfig(cfg)
+	iotClient := iot.NewFromConfig(cfg)
 
 	return &CogClient{
 		AppClientId: cognitoClientId,
 		UserPoolId:  userPoolId,
-		ClientCog:   cognito.New(sess),
-		Iots:        iot.New(sess),
+		ClientCog:   cognitoIdentityProviderClient,
+		StsSvc:      stsClient,
+		IotClient:   iotClient,
+		Cfg:         &cfg,
 	}
 
 }
 
 func (s *CogClient) GetCerds() (interface{}, error) {
-	thingName := "23F05110000126"
 
-	resp, err := s.Iots.ListThingPrincipals(&iot.ListThingPrincipalsInput{
-		ThingName: aws.String(thingName),
-	})
-
-	if err != nil {
-		panic(err)
-	}
-
-	pincipal := resp.Principals
-
-	fmt.Println(pincipal)
-
-	certificateId := extractCertificateId(*resp.Principals[0])
-
-	//authenticateCognito(s.ClientCog)
-	// res := &s.UserPoolId
-
-	return certificateId, nil
-}
-
-func authenticateCognito(client *cognito.CognitoIdentityProvider) {
-	// TODO: Implement Cognito authentication logic here
-
-	// poolID := "ap-southeast-1_EqxkPGgmk"
-	clientID := "qq74q62sm1jfg8t7qetmo3a86"
 	username := "tidosi6511@vaband.com"
 	password := "Pass@word2020"
 
-	authInput := &cognito.InitiateAuthInput{
-		AuthFlow: aws.String("USER_PASSWORD_AUTH"),
-		ClientId: aws.String(clientID),
-		AuthParameters: map[string]*string{
-			"USERNAME": aws.String(username),
-			"PASSWORD": aws.String(password),
-		},
-	}
-
-	authOutput, err := client.InitiateAuth(authInput)
-	if err != nil {
-		panic(err)
-	}
-
-	// Retrieve the Cognito session tokens
-	idToken := authOutput.AuthenticationResult.IdToken
-	accessToken := authOutput.AuthenticationResult.AccessToken
-
-	fmt.Println("idToken")
-	fmt.Println(idToken)
-	fmt.Println("AccessToken")
-	fmt.Println(accessToken)
-
-}
-
-func extractCertificateId(principalArn string) string {
-	arnParts := strings.Split(principalArn, "/")
-	return arnParts[len(arnParts)-1]
-}
-
-func getCredential() {
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		log.Fatalln("Failed to load AWS config:", err)
-	}
-
-	// Create a Cognito Identity Provider client
-	cognitoIdentityProviderClient := cognitoidentityprovider.NewFromConfig(cfg)
-
-	// Specify the Cognito user pool ID, username, and password
-	userPoolID := "YOUR_USER_POOL_ID"
-	username := "YOUR_USERNAME"
-	password := "YOUR_PASSWORD"
-
 	// Authenticate the user and retrieve the Cognito ID token
-	authResult, err := cognitoIdentityProviderClient.InitiateAuth(context.TODO(), &cognitoidentityprovider.InitiateAuthInput{
-		AuthFlow: cognitoidentityprovider.AuthFlowTypeUserPasswordAuth,
+	authResult, err := s.ClientCog.InitiateAuth(context.TODO(), &cip.InitiateAuthInput{
+		AuthFlow: types.AuthFlowTypeUserPasswordAuth,
 		AuthParameters: map[string]string{
 			"USERNAME": username,
 			"PASSWORD": password,
 		},
-		ClientId:   "YOUR_CLIENT_ID",
-		UserPoolId: userPoolID,
+		ClientId: aws.String(s.AppClientId),
+		// UserPoolId: aws.String(s.UserPoolId),
 	})
 	if err != nil {
 		log.Fatalln("Failed to authenticate user:", err)
 	}
 
-	// Create a Cognito Identity client
-	cognitoIdentityClient := cognitoidentity.NewFromConfig(cfg)
-
-	// Get the Cognito identity ID using the Cognito ID token
-	getIdentityResult, err := cognitoIdentityClient.GetId(context.TODO(), &cognitoidentity.GetIdInput{
-		IdentityPoolId: "YOUR_IDENTITY_POOL_ID",
-		Logins: map[string]string{
-			"cognito-idp.<YOUR_AWS_REGION>.amazonaws.com/<YOUR_USER_POOL_ID>": *authResult.AuthenticationResult.IdToken,
-		},
-	})
-	if err != nil {
-		log.Fatalln("Failed to get Cognito identity ID:", err)
-	}
-
+	//  Create a Cognito Identity client
 	// Create a Security Token Service (STS) client
-	stsClient := sts.NewFromConfig(cfg)
 
-	// Assume an IAM role with the Cognito identity as the role's principal
-	assumeRoleResult, err := stsClient.AssumeRoleWithWebIdentity(context.TODO(), &sts.AssumeRoleWithWebIdentityInput{
-		RoleArn:          "YOUR_ROLE_ARN",
-		RoleSessionName:  "session",
+	myRoleArn := aws.String("arn:aws:iam::513310385702:role/service-role/customer_air_iot_2023")
+
+	assumeRoleResult, err := s.StsSvc.AssumeRoleWithWebIdentity(context.TODO(), &sts.AssumeRoleWithWebIdentityInput{
+		RoleArn:          myRoleArn,
+		RoleSessionName:  aws.String("session"),
 		WebIdentityToken: authResult.AuthenticationResult.IdToken,
 	})
+
+	// if err != nil {
+	// 	println(err)
+	// 	panic(err)
+	// }
+
+	// creds := stscreds.NewAssumeRoleProvider(s.StsSvc, *myRoleArn)
+	// webId := stscreds.NewWebIdentityRoleProvider(s.StsSvc, *myRoleArn)
+
+	// _ = webId
+
+	println("Cert")
+	println(assumeRoleResult.Credentials)
+	// println(creds)
+
+	// s.Cfg.Credentials = aws.NewCredentialsCache(creds)
+
+	// cerds := stscreds.NewWebIdentityRoleProvider(s.StsSvc, *aws.String("arn:aws:iam::513310385702:role/service-role/customer_air_iot_2023"))
+	// s.Cfg.Credentials = aws.NewCredentialsCache(cerds)
+
+	// if err != nil {
+	// 	log.Fatalln("Failed to assume role with web identity:", err)
+	// }
+
+	// println("Assumrole")
+	// println(assumeRoleResult.Credentials)
+	// _ = assumeRoleResult.Credentials
+	// Set the temporary credentials in the AWS config
+	// s.Cfg.Credentials = aws.NewCredentialsCache(assumeRoleResult.Credentials)
+
+	// s.Cfg.Credentials = aws.NewCredentialsCache(assumeRoleResult.Credentials)
+	return authResult.AuthenticationResult, nil
+}
+
+func GetClientId(idToken string) (string, error) {
+
+	pubKeyURL := "https://cognito-idp.%s.amazonaws.com/%s/.well-known/jwks.json"
+	formattedURL := fmt.Sprintf(pubKeyURL, os.Getenv("AWS_REGION"), os.Getenv("USER_POOL_ID"))
+	keySet, err := jwk.Fetch(context.TODO(), formattedURL)
 	if err != nil {
-		log.Fatalln("Failed to assume role with web identity:", err)
+		return "", nil
+	}
+	token, err := jwt.Parse(
+		[]byte(idToken),
+		jwt.WithKeySet(keySet),
+		jwt.WithValidate(true),
+	)
+	if err != nil {
+		return "", nil
 	}
 
-	// Set the temporary credentials in the AWS config
-	cfg.Credentials = aws.NewCredentialsCache(assumeRoleResult.Credentials)
+	username, _ := token.Get("cognito:username")
+	cognitoIdentityId := username.(string)
 
-	// Create an IoT client using the temporary credentials
-	iotClient := iot.NewFromConfig(cfg)
-
-	// Specify the Thing name and Thing policy name or ARN
-	thingName := "YOUR_THING_NAME"
-	thingPolicyNameOrArn := "YOUR_THING_POLICY_NAME_OR_ARN"
-
-	// Attach the Thing policy to the Thing
-	// _, err = iotClient.AttachPolicy(context.TODO(), &iot.AttachPolicyInput{
-	// 	PolicyName: &})
-
+	return cognitoIdentityId, nil
 }
