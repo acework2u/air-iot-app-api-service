4eaa3e56091216e69c008a0b456fdb8d9ebf76c2
Anon D <acework2u@gmail.com>
2023-11-07 15:54:53 +0700

    update user api docs

--- handler/customer.go
+++ handler/customer.go
@@ -5,7 +5,6 @@ import (
 	"github.com/acework2u/air-iot-app-api-service/services"
 	"github.com/acework2u/air-iot-app-api-service/utils"
 	"github.com/gin-gonic/gin"
-	"net/http"
 	"time"
 )
 
@@ -46,45 +45,40 @@ func NewCustomerHandler(cusService services.CustomerService) CustomerHandler {
 // GetCustomer  godoc
 // @Summary  Get User info
 // @Description Return User Information
-// @Produce  application/json
-// @Security BearerAuth
 // @Tags  Users
+// @Produce json
+// @Security BearerAuth
 // @Success 200 {object} utils.ApiResponse{}
 // @Failure 400 {object} utils.ApiResponse{}
 // @Router  /my [get]
 func (h *CustomerHandler) GetCustomer(c *gin.Context) {
 
-	userToken, check := c.Get("UserToken")
+	_, check := c.Get("UserToken")
 	userId, _ := c.Get("UserId")
-
-	_ = userToken
 
 	if check {
 		result, err := h.cusService.CustomerById(userId.(string))
-
-		webres := &Response{
-			Status:  http.StatusBadRequest,
-			Message: "No Data",
-		}
-
 		if err != nil {
-
-			//response.Status = http.StatusBadRequest
-			//response.Message = "No data"
-			c.Header("Content-Type", "application/json")
-			c.JSON(http.StatusBadRequest, webres)
+			h.res.BadRequest(c, "no data")
 			return
 		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"status":  http.StatusOK,
-			"message": result,
-		})
+		h.res.Success(c, result)
 
 	}
 
 }
 
+// GetCustomerById godoc
+// @Summary get user information by id
+// @Description get user information by id
+// @Tags Users
+// @Accept       json
+// @Produce      json
+// @Security BearerAuth
+// @Param        id   path      int  true  "Account ID"
+// @Success 200 {object} utils.ApiResponse{}
+// @Failure 400 {object} utils.ApiResponse{}
+// @Router  /my/{id} [get]
 func (h *CustomerHandler) GetCustomerById(c *gin.Context) {
 
 	ReqId := c.Param("id")
@@ -94,188 +88,130 @@ func (h *CustomerHandler) GetCustomerById(c *gin.Context) {
 		if ReqId == userId {
 			result, err := h.cusService.CustomerById(userId.(string))
 			if err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{
-					"status":  http.StatusBadRequest,
-					"message": "no data",
-				})
+				h.res.BadRequest(c, "no data")
 				return
 			}
-
-			c.JSON(http.StatusOK, gin.H{
-				"status":  http.StatusOK,
-				"message": result,
-			})
-
+			h.res.Success(c, result)
 			return
 
 		}
-
-		c.JSON(http.StatusBadRequest, gin.H{
-			"status":  http.StatusBadRequest,
-			"message": "id is required",
-		})
+		h.res.BadRequest(c, "field user id is required")
 		return
 	}
 }
-func (h *CustomerHandler) PostCustomer(ctx *gin.Context) {
+
+// NewCustomerById godoc
+// @Summary new user information
+// @Description New user information
+// @Tags Users
+// @Accept       json
+// @Produce      json
+// @Security BearerAuth
+// @Param        id   path      int  true  "Account ID"
+// @Success 200 {object} utils.ApiResponse{}
+// @Failure 400 {object} utils.ApiResponse{}
+// @Router  /my [post]
+
+func (h *CustomerHandler) PostCustomer(c *gin.Context) {
 
 	var customer *services.CreateCustomerRequest
 
-	if err := ctx.ShouldBindJSON(&customer); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	if err := c.ShouldBindJSON(&customer); err != nil {
+		h.res.BadRequest(c, err.Error())
 		return
 	}
 
 	response, err := h.cusService.CreateNewCustomer(customer)
 
 	if err != nil {
-
-		msgFailerd := &utils.ApiResponse{
-			Status:  http.StatusBadRequest,
-			Message: err.Error(),
-		}
-
-		utils.ResponseSuccess(ctx, msgFailerd)
+		h.res.BadRequest(c, err.Error())
 		return
 	}
-
-	ctx.JSON(http.StatusCreated, gin.H{
-		"status":  "successful",
-		"message": "Create Customer",
-		"data":    response,
-	})
-
+	h.res.Success(c, response)
 }
+
+// UpdateCustomer godoc
+// @Summary update user information
+// @Description post method user information update
+// @Tags Users
+// @Security BearerAuth
+// @Param userInfo body services.UpdateInfoRequest true "User information"
+// @Success 200 {object} utils.ApiResponse{}
+// @Failure 400 {object} utils.ApiResponse{}
+// @Router /my/info [post]
 func (h *CustomerHandler) UpdateCustomer(c *gin.Context) {
 
 	cusID := c.Query("id")
 	userId, ok := c.Get("UserId")
-
-	infoUpdate := services.UpdateInfoRequest{}
-
+	infoUpdate := &services.UpdateInfoRequest{}
 	if ok {
-
 		if cusID == userId {
-
-			if err := c.ShouldBindJSON(&infoUpdate); err != nil {
-
-				c.JSON(http.StatusBadRequest, gin.H{
-					"status":  http.StatusBadRequest,
-					"message": err.Error(),
-				})
+			if err := c.ShouldBindJSON(infoUpdate); err != nil {
+				h.res.BadRequest(c, err.Error())
 				return
 			}
+
 			infoUpdate.UpdateAt = time.Now()
-
 			updateId := userId.(string)
-
-			resInfo, ok := h.cusService.UpdateCustomer(updateId, &infoUpdate)
+			_, ok := h.cusService.UpdateCustomer(updateId, infoUpdate)
 
 			if ok != nil {
-				fmt.Println("In handler error")
-				fmt.Println(ok.Error())
+				h.res.BadRequest(c, ok.Error())
+				return
 			}
-
-			_ = resInfo
-
-			c.JSON(http.StatusOK, gin.H{
-				"status":  http.StatusOK,
-				"message": infoUpdate,
-			})
+			h.res.Success(c, infoUpdate)
 
 		} else {
-
 			// userId do not match
-			c.JSON(http.StatusBadRequest, gin.H{
-				"status":  http.StatusBadRequest,
-				"message": "Access don't allowed",
-			})
+			h.res.BadRequest(c, "Access don't allowed")
 			return
 		}
 
 	}
 
-	//
-	//_ = custId
-	//
-	////var customer *interface{}
-	//
-	//var customer *services.UpdateCustomer
-	//err := ctx.ShouldBindJSON(&customer)
-	//if err != nil {
-	//	// utils.StatusBadRequest("Data no match")
-	//	ctx.JSON(http.StatusBadGateway, gin.H{
-	//		"status":  http.StatusBadRequest,
-	//		"message": err,
-	//	})
-	//
-	//	return
-	//}
-	//
-	//if len(customer.Name) == 0 {
-	//	ctx.JSON(http.StatusBadRequest, gin.H{
-	//		"status":  http.StatusBadRequest,
-	//		"message": "this len",
-	//	})
-	//	// utils.StatusBadRequest("Name is required")
-	//	return
-	//}
-	//
-	//doc, err := h.cusService.UpdateCustomer(custId, customer)
-	//
-	////doc, err := utils.ToDoc(customer)
-	//
-	//if err != nil {
-	//	ctx.JSON(http.StatusBadRequest, gin.H{
-	//		"status":  http.StatusBadRequest,
-	//		"message": err.Error(),
-	//	})
-	//	return
-	//}
-	//
-	//ctx.JSON(http.StatusAccepted, gin.H{
-	//	"status":  http.StatusAccepted,
-	//	"message": "Update Success",
-	//	"data":    doc,
-	//})
 }
 
-func (h *CustomerHandler) DelCustomer(ctx *gin.Context) {
-	delID := ctx.Param("id")
+// DelCustomer godoc
+// @Summary Delete user by id
+// @Description Delete user by id
+// @Tags Users
+// @Security BearerAuth
+// @Param        id   path      int  true  "Account ID"
+// @Success 200 {object} utils.ApiResponse{}
+// @Failure 400 {object} utils.ApiResponse{}
+// @Router /my/{id} [delete]
+func (h *CustomerHandler) DelCustomer(c *gin.Context) {
+	delID := c.Param("id")
 
 	if len(delID) == 0 {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"status":  http.StatusBadRequest,
-			"message": "id is required",
-		})
+		h.res.BadRequest(c, "field id is required")
 		return
 	}
-
 	err := h.cusService.DeleteCustomer(delID)
-
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{
-			"status":  http.StatusBadRequest,
-			"message": err.Error(),
-		})
+		h.res.BadRequest(c, err.Error())
 		return
 	}
+	txtDel := fmt.Sprintf("delete is id %v successful", delID)
+	h.res.Success(c, txtDel)
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"status":  http.StatusOK,
-		"message": delID,
-	})
 }
 
+// PostNewAddress godoc
+// @Summary PostNewAddress user by id
+// @Description PostNewAddress user by id
+// @Tags Users
+// @Security BearerAuth
+// @Param CustomerAddress body services.CustomerAddress true "Customer Address"
+// @Success 200 {object} utils.ApiResponse{}
+// @Failure 400 {object} utils.ApiResponse{}
+// @Router /my/address [post]
 func (h *CustomerHandler) PostNewAddress(c *gin.Context) {
 	userId, _ := c.Get("UserId")
 	var addressInfo *services.CustomerAddress
 	err := c.ShouldBindJSON(&addressInfo)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"status":  http.StatusBadRequest,
-			"message": err.Error(),
-		})
+		h.res.BadRequest(c, err.Error())
 		return
 	}
 	addressInfo.CustomerId = userId.(string)
@@ -283,18 +219,24 @@ func (h *CustomerHandler) PostNewAddress(c *gin.Context) {
 	// Insert to DB
 	resp, err := h.cusService.CustomerNewAddress(addressInfo)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"status":  http.StatusBadRequest,
-			"message": err.Error(),
-		})
+		h.res.BadRequest(c, err.Error())
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{
-		"status":  http.StatusOK,
-		"message": resp,
-	})
+	h.res.Success(c, resp)
 }
+
+// UpdateAddress godoc
+// @Summary Update user address
+// @Description Update
+// @Tags Users
+// @Produce json
+// @Param        id   path      int  true  "Account ID"
+// @Param address body services.CustomerAddress true "Address information"
+// @Success 200 {object} utils.ApiResponse{}
+// @Failure 400 {object} utils.ApiResponse{}
+// @Router /my/{id} [put]
 func (h *CustomerHandler) UpdateAddress(c *gin.Context) {
+
 	userId, _ := c.Get("UserId")
 	addressInfo := services.CustomerAddress{}
 	filter := services.Filter{}
@@ -303,10 +245,7 @@ func (h *CustomerHandler) UpdateAddress(c *gin.Context) {
 	err := c.ShouldBindJSON(&addressInfo)
 
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"status":  http.StatusBadRequest,
-			"message": err.Error(),
-		})
+		h.res.BadRequest(c, err.Error())
 		return
 	}
 
@@ -319,29 +258,5 @@ func (h *CustomerHandler) UpdateAddress(c *gin.Context) {
 		return
 	}
 	fmt.Println(cusAddr)
-
 	h.res.Success(c, cusAddr)
 }
-
-//func AcceptAnyValue(arg any) {
-//	switch v := arg.(type) {
-//	case string:
-//		fmt.Printf("String: %s", v)
-//	case int:
-//		fmt.Printf("Int32: %d", v)
-//	case float64:
-//		fmt.Printf("float64: %f", v)
-//	case map[string]int:
-//		fmt.Printf("map[string]int: %+v", v)
-//	case map[int]string:
-//		fmt.Printf("map[int]string: %+v", v)
-//	case map[string]map[any]any:
-//		fmt.Printf("map[string]map[any]any: %+v", v)
-//	case []int:
-//		fmt.Printf("[]int: %+v", v)
-//	default:
-//		fmt.Printf("Undefined type: %s", reflect.TypeOf(v))
-//	}
-//
-//	fmt.Println()
-//}
